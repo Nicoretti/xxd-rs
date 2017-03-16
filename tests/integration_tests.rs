45ff52a4270fc97e4526e8b830bcd1600c4119cd
Nicola Coretti <nico.coretti@gmail.com>
2017-03-16 17:31:39 +0100

    Refactored command availability check
    * Refactored command availablility check
    * Removed unnecessary test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,23 +1,13 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 fn is_command_available(command: String) -> bool {
-    // TODO NiCo: remove output on terminal for this check
-    let mut result = Command::new(command)
-                         .arg("-v")
-                         .status();
-    match result {
-        Ok(status) => true,
-        Err(_) => false,
+    match Command::new(command).arg("-v").stdout(Stdio::null()).status() {
+        Ok(status) => status.success(),
+        _ => false,
     }
 }
 
-// Attention: if this test fails all other test will/should do so too
 #[test]
 fn original_xxd_is_available() {
     assert!(is_command_available("xxd".to_string()));
-}
-
-#[test]
-fn xxd_rs_is_available() {
-    assert!(is_command_available("xxd-rs".to_string()));
 }
