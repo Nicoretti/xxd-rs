ac742043530a8ce4fab3e0287c70bdfd217bee55
Nicola Coretti <nico.coretti@gmail.com>
2017-03-21 20:42:39 +0100

    Refactor OutputLine to use OutputSettings

--- src/xxd/dump.rs
+++ src/xxd/dump.rs
@@ -11,6 +11,7 @@ pub enum OutputFormat {
     Binary,
 }
 
+#[derive(Debug)]
 pub struct OutputSettings {
     start_address: u32,
     show_address: bool,
@@ -63,53 +64,37 @@ impl OutputSettings {
     }
 }
 
-
 /// The `OutputLine` struct contains all  information needed to dump/output a single line of data.
 #[derive(Debug)]
 pub struct OutputLine<'a> {
-    start_address: u32,
-    show_address: bool,
+    output_settings: OutputSettings,
     data: &'a [u8], // TODO NiCo: Add member for data format (output format of data)
-    group_size: usize,
-    columns: usize,
-    show_interpretation: bool,
-    output_fmt: OutputFormat,
 }
 
 impl<'a> OutputLine<'a> {
     fn new(data: &[u8]) -> OutputLine {
         OutputLine {
-            start_address: 0,
-            show_address: true,
+            output_settings: OutputSettings::new(),
             data: data,
-            group_size: 1,
-            columns: 8,
-            show_interpretation: true,
-            output_fmt: OutputFormat::Hex,
         }
     }
 
-    pub fn format(self, fmt: OutputFormat) -> Self {
+    pub fn format(self, settings: OutputSettings) -> Self {
         OutputLine {
-            start_address: self.start_address,
-            show_address: self.show_interpretation,
+            output_settings: settings,
             data: self.data,
-            group_size: self.group_size,
-            columns: self.columns,
-            show_interpretation: self.show_interpretation,
-            output_fmt: fmt,
         }
     }
 
     fn write_address(&self, f: &mut ::fmt::Formatter) -> Result<()> {
-        write!(f, "{:08.X}: ", self.start_address).map_err(|e| e.into())
+        write!(f, "{:08.X}: ", self.output_settings.start_address).map_err(|e| e.into())
     }
 
     fn write_bytes(&self, f: &mut ::fmt::Formatter) -> Result<()> {
         let mut byte_count = 0;
         for b in self.data.iter() {
             byte_count += 1;
-            let is_seperator_necessary = byte_count % self.group_size == 0;
+            let is_seperator_necessary = byte_count % self.output_settings.group_size == 0;
             if is_seperator_necessary {
                 self.write_formated_byte(f, b)?;
                 write!(f, " ")?
@@ -121,7 +106,7 @@ impl<'a> OutputLine<'a> {
     }
 
     fn write_formated_byte(&self, f: &mut ::fmt::Formatter, byte: &u8) -> Result<()> {
-        match self.output_fmt {
+        match self.output_settings.output_fmt {
             OutputFormat::Hex => write!(f, "{:02.X}", byte).map_err(|e| e.into()),
             OutputFormat::Octal => write!(f, "{:02.o}", byte).map_err(|e| e.into()),
             OutputFormat::Decimal => write!(f, "{:02}", byte).map_err(|e| e.into()),
@@ -143,11 +128,11 @@ impl<'a> OutputLine<'a> {
 
 impl<'a> ::fmt::Display for OutputLine<'a> {
     fn fmt(&self, f: &mut ::fmt::Formatter) -> ::fmt::Result {
-        if self.show_address {
+        if self.output_settings.show_address {
             self.write_address(f);
         }
         self.write_bytes(f);
-        if self.show_interpretation {
+        if self.output_settings.show_interpretation {
             self.write_interpretation(f);
         }
         Ok(())
@@ -211,7 +196,8 @@ mod test {
     fn octal_output_format_on_a_single_line() {
         let data = [8, 9, 10, 11, 12, 13, 14, 15];
         let expected_output = "00000000: 10 11 12 13 14 15 16 17    ........";
-        let output_line = OutputLine::new(&data).format(OutputFormat::Octal);
+        let output_settings = OutputSettings::new().format(OutputFormat::Octal);
+        let output_line = OutputLine::new(&data).format(output_settings);
         let mut buffer = String::new();
         let result = write!(&mut buffer, "{}", output_line);
         assert_eq!(Ok(()), result);
@@ -222,7 +208,8 @@ mod test {
     fn binary_output_format_on_a_single_line() {
         let data = [65, 66, 67, 68, 126, 124, 60, 46];
         let expected_output = "00000000: 1000001 1000010 1000011 1000100 1111110 1111100 111100 101110    ABCD~|<.";
-        let output_line = OutputLine::new(&data).format(OutputFormat::Binary);
+        let output_settings = OutputSettings::new().format(OutputFormat::Binary);
+        let output_line = OutputLine::new(&data).format(output_settings);
         let mut buffer = String::new();
         let result = write!(&mut buffer, "{}", output_line);
         assert_eq!(Ok(()), result);
@@ -233,7 +220,8 @@ mod test {
     fn interpretation_for_default_settings() {
         let data = [65, 66, 67, 68, 126, 124, 60, 46];
         let expected_output = "00000000: 41 42 43 44 7E 7C 3C 2E    ABCD~|<.";
-        let output_line = OutputLine::new(&data);
+        let output_settings = OutputSettings::new();
+        let output_line = OutputLine::new(&data).format(output_settings);
         let mut buffer = String::new();
         let result = write!(&mut buffer, "{}", output_line);
         assert_eq!(Ok(()), result);
