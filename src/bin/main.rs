0bf1b7abf54283c51a0f0a4bba92c97c6095ff74
Nicola Coretti <nico.coretti@gmail.com>
2017-04-06 20:23:29 +0200

    Create cli module

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,12 +3,13 @@ extern crate clap;
 extern crate error_chain;
 extern crate xxd;
 
-use clap::{Arg, ArgMatches, App, SubCommand, AppSettings};
+use clap::ArgMatches;
 use std::process::exit;
 use std::fmt::Display;
 use std::fs::File;
 use std::io::Read;
 
+mod cli;
 mod errors {
     error_chain! {
         foreign_links {
@@ -22,7 +23,7 @@ mod errors {
 use errors::*;
 
 fn main() {
-    let matches = create_arg_parser().get_matches();
+    let matches = cli::create_arg_parser().get_matches();
     match run(&matches) {
         Ok(_) => exit(0),
         Err(e) => {
@@ -116,99 +117,4 @@ fn command_not_supported() -> Result<()> {
 
 fn report_error<T: Display>(error: &T) {
     println!("xxd-rs: {}", error)
-}
-
-fn create_arg_parser<'a, 'b>() -> App<'a, 'b> {
-    App::new("A rust based clone of the all time classic xxd tool")
-        .version("0.1.0")
-        .author("Nicola Coretti <nicola.coretti@gmail.com>")
-        .about("make a hexdump or the reverse")
-        .global_settings(&[AppSettings::ColoredHelp])
-        .arg(Arg::with_name("infile")
-                 .required(false)
-                 .global(true)
-                 .index(1)
-                 .help("Input file which shall be used (default: stdin)"))
-        .arg(Arg::with_name("outfile")
-                 .required(false)
-                 .global(true)
-                 .index(2)
-                 .help("File to which the output will be written (default: stdout)"))
-        .arg(Arg::with_name("seek")
-                 .short("s")
-                 .long("seek")
-                 .required(false)
-                 .takes_value(true)
-                 .global(true)
-                 .help("Offset in the file where to start reading"))
-        .arg(Arg::with_name("length")
-                 .short("l")
-                 .long("length")
-                 .required(false)
-                 .takes_value(true)
-                 .global(true)
-                 .help("Amount of bytes which shall be read"))
-        .subcommand(SubCommand::with_name("dump")
-                        .about("Dumps an input file in the appropriate output format")
-                        .arg(Arg::with_name("format")
-                                 .short("f")
-                                 .long("format")
-                                 .required(false)
-                                 .takes_value(true)
-                                 .possible_value("hex")
-                                 .possible_value("bin")
-                                 .possible_value("oct")
-                                 .possible_value("dec")
-                                 .help("Specifies the output format for the value (default: hex)"))
-                        .arg(Arg::with_name("group-size")
-                                 .short("g")
-                                 .long("group-size")
-                                 .required(false)
-                                 .takes_value(true)
-                                 .help("Separate  the output of every <bytes> bytes (two hex characters or eight bit-digits each) by a whitespace."))
-                        .arg(Arg::with_name("columns")
-                                 .short("c")
-                                 .long("columns")
-                                 .required(false)
-                                 .takes_value(true)
-                                 .help("Specifies the amount of output columns")))
-        .subcommand(SubCommand::with_name("convert")
-                        .about("Converts input data to a file (e.g. hexstream -> binary file")
-                        .arg(Arg::with_name("format")
-                                 .short("f")
-                                 .long("format")
-                                 .required(false)
-                                 .takes_value(true)
-                                 .possible_value("binary")
-                                 .possible_value("c-array")
-                                 .possible_value("oct")
-                                 .help("Specifies the output format (default: hex)")))
-        .subcommand(SubCommand::with_name("generate")
-                        .about("Generates a source file containing the specified file as array")
-                        .arg(Arg::with_name("template")
-                                 .short("t")
-                                 .long("template")
-                                 .required(false)
-                                 .takes_value(true)
-                                 .possible_value("c")
-                                 .possible_value("cpp")
-                                 .possible_value("rs")
-                                 .help("Specifies a template file which shall be used for \
-                                        generation"))
-                        .arg(Arg::with_name("format")
-                                 .short("f")
-                                 .long("format")
-                                 .required(false)
-                                 .takes_value(true)
-                                 .possible_value("hex")
-                                 .possible_value("oct")
-                                 .possible_value("dec")
-                                 .possible_value("bin")
-                                 .help("Specifies the output format (default: hex)"))
-                        .arg(Arg::with_name("Separator")
-                                 .long("separator")
-                                 .required(false)
-                                 .takes_value(true)
-                                 .help("Specifies the the separator between single values \
-                                        (default: ',')")))
 }
