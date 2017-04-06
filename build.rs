6dc04e533d8cc8847296936ace0b7826f885ee6d
Nicola Coretti <nico.coretti@gmail.com>
2017-04-06 20:40:42 +0200

    Generate zsh complition as part of the build

---
+++ build.rs
@@ -0,0 +1,15 @@
+extern crate clap;
+
+use clap::Shell;
+use std::env;
+
+include!("src/bin/cli.rs");
+
+fn main() {
+    let outdir = match env::var_os("PWD") {
+        None => return,
+        Some(outdir) => outdir,
+    };
+    let mut app = create_arg_parser();
+    app.gen_completions("xxd-rs", Shell::Zsh, outdir);
+}
