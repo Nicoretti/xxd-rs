b4098835a534094c97ba8452b8f75328017b3822
Nicola Coretti <nico.coretti@gmail.com>
2017-04-06 19:50:11 +0200

    Add decimal output format

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -157,6 +157,7 @@ fn create_arg_parser<'a, 'b>() -> App<'a, 'b> {
                                  .possible_value("hex")
                                  .possible_value("bin")
                                  .possible_value("oct")
+                                 .possible_value("dec")
                                  .help("Specifies the output format for the value (default: hex)"))
                         .arg(Arg::with_name("group-size")
                                  .short("g")
