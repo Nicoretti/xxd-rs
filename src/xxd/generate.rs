ec401cd46f553da860e05c797813003cf79af1bd
Nicola Coretti <nico.coretti@gmail.com>
2017-04-25 20:08:43 +0200

    Add bytes_per_line setting

--- src/xxd/generate.rs
+++ src/xxd/generate.rs
@@ -31,6 +31,7 @@ struct Template {
     prefix: String,
     separator: String,
     suffix: String,
+    bytes_per_line: usize,
 }
 
 impl Template {
@@ -41,6 +42,7 @@ impl Template {
                     prefix: C_PRE.to_string(),
                     separator: C_SEPERATOR.to_string(),
                     suffix: C_POST.to_string(),
+                    bytes_per_line: 16,
                 }
             }
             Language::Cpp => {
@@ -48,6 +50,7 @@ impl Template {
                     prefix: CPP_PRE.to_string(),
                     separator: CPP_SEPERATOR.to_string(),
                     suffix: CPP_POST.to_string(),
+                    bytes_per_line: 16,
                 }
             }
             Language::Python => {
@@ -55,6 +58,7 @@ impl Template {
                     prefix: PYTHON_PRE.to_string(),
                     separator: PYTHON_SEPERATOR.to_string(),
                     suffix: PYTHON_POST.to_string(),
+                    bytes_per_line: 16,
                 }
             }
             Language::Rust => {
@@ -62,6 +66,7 @@ impl Template {
                     prefix: RUST_PRE.to_string(),
                     separator: RUST_SEPERATOR.to_string(),
                     suffix: RUST_POST.to_string(),
+                    bytes_per_line: 16,
                 }
             }
         }
@@ -78,6 +83,9 @@ impl Render for Template {
                 output = output + &format!("{}", byte);
             } else {
                 output = output + &format!("{}{} ", byte, self.separator);
+                if (index + 1) % self.bytes_per_line == 0 {
+                    output = output + &"\n";
+                }
             }
         }
         output = output + &self.suffix;
@@ -97,10 +105,28 @@ mod test {
     }
 
     #[test]
+    fn render_c_template_with_large_data() {
+        let data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+                    22, 23, 24, 25, 26];
+        let template = Template::new(Language::C);
+        let expected_result = "const char data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, \n16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26};";
+        assert_eq!(template.render(&data), expected_result);
+    }
+
+    #[test]
     fn render_basic_cplusplus_template() {
         let data = [0, 1, 2, 3, 4, 5];
         let template = Template::new(Language::Cpp);
         let expected_result = "const char data[] = {0, 1, 2, 3, 4, 5};";
+        assert_eq!(template.render(&data), expected_result);
+    }
+
+    #[test]
+    fn render_cplusplus_template_with_large_data() {
+        let data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+                    22, 23, 24, 25, 26];
+        let template = Template::new(Language::Cpp);
+        let expected_result = "const char data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, \n16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26};";
         assert_eq!(template.render(&data), expected_result);
     }
 
@@ -113,10 +139,28 @@ mod test {
     }
 
     #[test]
+    fn render_python_template_with_large_data() {
+        let data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+                    22, 23, 24, 25, 26];
+        let template = Template::new(Language::Python);
+        let expected_result = "data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, \n16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]";
+        assert_eq!(template.render(&data), expected_result);
+    }
+
+    #[test]
     fn render_basic_rust_template() {
         let data = [0, 1, 2, 3, 4, 5];
         let template = Template::new(Language::Rust);
         let expected_result = "let data = [0, 1, 2, 3, 4, 5];";
+        assert_eq!(template.render(&data), expected_result);
+    }
+
+    #[test]
+    fn render_rust_template_with_large_data() {
+        let data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+                    22, 23, 24, 25, 26];
+        let template = Template::new(Language::Rust);
+        let expected_result = "let data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, \n16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];";
         assert_eq!(template.render(&data), expected_result);
     }
 }
